2a5d4bac6b1d9197decbf5dac1388c93869a890a
LongWei <yilang2007lw@gmail.com>
2013-08-18 16:42:38 +0800

    add main activity

--- src/com/yilang/yyhs/StartActivity.java
+++ src/com/yilang/yyhs/StartActivity.java
@@ -1,14 +1,54 @@
 package com.yilang.yyhs;
 
+import com.yilang.yyhs.ui.MainActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 
 public class StartActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_start);
+		final View view = View.inflate(this, R.layout.activity_start, null);
+		setContentView(view);
+
+		AlphaAnimation animation = new AlphaAnimation(0.1f, 1.0f);
+		animation.setDuration(2000);
+		view.startAnimation(animation);
+
+		animation.setAnimationListener(new AnimationListener() {
+
+			@Override
+			public void onAnimationEnd(Animation arg0) {
+				// TODO Auto-generated method stub
+				startMain();
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onAnimationStart(Animation arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+		});
+
+	}
+
+	private void startMain() {
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+		finish();
 	}
 }
---
+++ src/com/yilang/yyhs/ui/MainActivity.java
@@ -0,0 +1,17 @@
+package com.yilang.yyhs.ui;
+
+import com.yilang.yyhs.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
