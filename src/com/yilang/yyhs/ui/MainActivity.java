59f72d5444942dc9e1f0cd7ca9512ec6d8b05671
LongWei <yilang2007lw@gmail.com>
2013-08-18 19:43:10 +0800

    let main footer show

--- src/com/yilang/yyhs/ui/MainActivity.java
+++ src/com/yilang/yyhs/ui/MainActivity.java
@@ -3,7 +3,6 @@ package com.yilang.yyhs.ui;
 import com.yilang.yyhs.R;
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends Activity {
 
